f786380afb3fb5a6fac1543fa6ee1bfb51c7b9d7
R-B-Suraj <rb.suraj1kanha@gmail.com>
2024-05-17 15:37:23 +0530

    serving content, cache first strategy

--- serviceworker.js
+++ serviceworker.js
@@ -1,14 +1,39 @@
-const assets = ["/","style.css","app.js","sw-register.js",
-    "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+const assets = [
+    '/',
+    'style.css',
+    'app.js',
+    'sw-register.js',
+    'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
 ]
 
-// storing assets
-self.addEventListener("install", (event)=>{
+self.addEventListener('install', (event) => {
     // browser kills within 40s. if there's a large file it may not cache, so waitUntill
     event.waitUntil(
-        caches.open("assets").then(cache=>{
-            cache.addAll(assets)
+        caches.open('assets').then((cache) => {
+            try {
+                cache.addAll(assets)
+            } catch (e) {
+                console.log('error in caching asset')
+            }
         })
     )
 })
 
+// serve content
+// Cache first strategy
+self.addEventListener('fetch', (event) => {
+    if (event.request.url == 'http://127.0.0.1:5500/fake') {
+        const response = new Response(
+            `Dummy Response from ${event.request.url}`
+        )
+        event.respondWith(response)
+    } else {
+        event.respondWith(
+            caches
+                .match(event.request) // searching in the cache
+                .then((response) => {
+                    return response || fetch(event.request)
+                })
+        )
+    }
+})
