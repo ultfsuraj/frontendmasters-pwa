9e910eeac6c4aa952402c4abfab9d0a975a2617a
R-B-Suraj <rb.suraj1kanha@gmail.com>
2024-05-17 13:37:47 +0530

    caching assets using service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,14 @@
-// check in console. change top -> service worker 
-console.log('message from service worker')
+const assets = ["/","style.css","app.js","sw-register.js",
+    "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+]
+
+// storing assets
+self.addEventListener("install", (event)=>{
+    // browser kills within 40s. if there's a large file it may not cache, so waitUntill
+    event.waitUntill(
+        caches.open("assets").then(cache=>{
+            cache.addAll(assets)
+        })
+    )
+})
+
